cec0630779d52ee3ac49adc78f111eba45a7b385
jbn-1or <https://github.com/jbn-1or>
2026-07-10 00:15:29 +0800

    finish: book1 image12

--- src/tools/color.rs
+++ src/tools/color.rs
@@ -7,6 +7,13 @@ use image::Rgb;
 
 pub type Color = Vec3;
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        return linear_component.sqrt();
+    }
+    0.0
+}
+
 impl Color {
     /// 将像素颜色值写入 RGB 像素缓冲区
     /// # 参数`pixel_color`-像素颜色向量（各分量在[0,1]范围） `pixel`-目标RGB像素的可变引用
@@ -14,6 +21,22 @@ impl Color {
         let x = pixel_color.x();
         let y = pixel_color.y();
         let z = pixel_color.z();
+
+        let intensity: Interval = Interval::new(0.000, 0.999);
+        let r = (256.0 * intensity.clamp(x)) as u32;
+        let g = (256.0 * intensity.clamp(y)) as u32;
+        let b = (256.0 * intensity.clamp(z)) as u32;
+        *pixel = image::Rgb([r as u8, g as u8, b as u8]);
+    }
+
+    pub fn write_color_gamma(pixel_color: Color, pixel: &mut Rgb<u8>) {
+        let mut x = pixel_color.x();
+        let mut y = pixel_color.y();
+        let mut z = pixel_color.z();
+
+        x = linear_to_gamma(x);
+        y = linear_to_gamma(y);
+        z = linear_to_gamma(z);
 
         let intensity: Interval = Interval::new(0.000, 0.999);
         let r = (256.0 * intensity.clamp(x)) as u32;
