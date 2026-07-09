a22936ee7667fe079f6462c840d1b0607317a718
jbn-1or <https://github.com/jbn-1or>
2026-07-09 21:11:51 +0800

    fix: book1 image8

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ fn main() {
     // book1::image4::render();
     // book1::image5::render();
     // book1::image6::render();
-    book1::image7::render();
-    // book1::image8::render();
+    // book1::image7::render();
+    book1::image8::render();
     // book1::image9::render();
     // book1::image10::render();
     // book1::image11::render();
